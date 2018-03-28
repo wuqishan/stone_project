7ef123375205673790154a5eab760f2a3e2be62c
wells <qishan.wu.1@lexisnexis.com>
2018-03-28 15:17:51 +0800

    refs: 初始化

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'admin';
+    }
+
+    public function hello()
+    {
+        return 'hello world';
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return view();
+    }
+
+    public function productsGrid()
+    {
+        return view();
+    }
+
+    public function productsList()
+    {
+        return view();
+    }
+
+    public function detail($id)
+    {
+//        echo $id;
+        return view();
+    }
+}
